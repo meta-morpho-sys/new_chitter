62b4333cf3e7a1bcb4d6127e0e34686418f0af81
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-12 19:34:15 +0100

    Table is created conditionally

--- db/migrations/00001_users.rb
+++ db/migrations/00001_users.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
+table_name = :users
+
+unless DB.table_exists? table_name
+puts "Creating table >> #{table_name}"
+
 Sequel.migration do
-  change do
-    create_table :users do
-      primary_key :id
-      String :name
-      String :email, size: 320, null: false, unique: true
-      String :hashed_pswd, null: false
+    change do
+      create_table table_name do
+        primary_key :id
+        String :name
+        String :email, size: 320, null: false, unique: true
+        String :hashed_pswd, null: false
+      end
     end
   end
 end
