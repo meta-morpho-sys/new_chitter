210d95aa285b97c175fc3c7acd55a78e87d7cb92
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-16 10:55:15 +0100

    Clarification of the purpose of the partially shared code.

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -3,11 +3,15 @@
 Rake.application.load_rakefile
 
 RSpec.configure do |c|
+  # Makes sure the structure is set up and empty
+  # before running the suite of tests.
   c.before(:suite) do
     puts 'Cleaning databases.'
     Rake::Task['db:create'].execute
     DB[:users].truncate
   end
+  # The following will roll back the transactions
+  # after each example has been run.
   c.around(:example, :db) do |example|
     puts 'Rolling back transaction'
     DB.transaction(rollback: :always) { example.run }
