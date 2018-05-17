82fd94c88d7cd0c5bb59afe034f1bb3c889ea1d1
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-17 10:22:59 +0100

    Introduced web helpers.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
 # require our Sinatra app file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
+require_relative 'features/web_helpers'
 
 RSpec.configure do |config|
   config.order = :random
