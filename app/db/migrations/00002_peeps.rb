19b29d915128079712952f6fe90a7daa8baca025
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-22 12:39:07 +0100

    Peeps table

---
+++ app/db/migrations/00002_peeps.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+table_name = :peeps
+
+unless DB.table_exists? table_name
+  puts "Creating table >> #{table_name}"
+
+  Sequel.migration do
+    change do
+      create_table table_name do
+        primary_key :id
+        foreign_key :user_id, :users, on_delete: :cascade
+        String :text
+        DateTime :datetime
+      end
+    end
+  end
+end
+
