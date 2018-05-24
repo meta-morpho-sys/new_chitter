0b900cc1c71d99253684a3c3ae12ed5762e4b432
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-24 12:59:21 +0100

    Tidying up.

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,6 +2,8 @@
 
 # Manages Peep object interactions with the DB.
 class Peep
+  PEEPS_DS = DB[:peeps]
+
   attr_reader :id, :user_id, :text, :timestamp
 
   def initialize(id: nil, user_id: nil, text: nil, created_at: nil)
@@ -12,13 +14,13 @@ class Peep
   end
 
   def self.create(user_id, text)
-    id = DB[:peeps].insert(user_id: user_id, text: text, created_at: Time.now)
-    timestamp = DB[:peeps].select(:created_at).first
-    Peep.new(id: id, user_id: user_id, text: text, created_at: timestamp[:created_at])
+    id = PEEPS_DS.insert(user_id: user_id, text: text, created_at: Time.now)
+    res = PEEPS_DS.select(:created_at).first
+    Peep.new(id: id, user_id: user_id, text: text, created_at: res[:created_at])
   end
 
   def self.find(peep_id)
-    peep = DB[:peeps].where(id: peep_id).first
+    peep = PEEPS_DS.where(id: peep_id).first
     Peep.new(peep)
   end
 
