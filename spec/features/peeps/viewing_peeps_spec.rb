0a470ad91c4b8f33e79317992eaac8e92c87bd1c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-05-27 13:45:45 +0100

    Test to view exactly the amount of created peeps.

--- spec/features/peeps/viewing_peeps_spec.rb
+++ spec/features/peeps/viewing_peeps_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 feature 'Viewing peeps', :db do
-  scenario 'a user can see all her peeps' do
+  scenario 'a user can see their peeps sorted with most recent first' do
     sign_up
 
     expect(current_path).to eq '/user/1/peeps'
@@ -12,5 +12,17 @@ feature 'Viewing peeps', :db do
 
     expect('Test3 peep').to appear_before 'Test2 peep'
     expect('Test2 peep').to appear_before 'Test peep'
+  end
+
+  scenario 'a user can see exactly the number of peeps they have created' do
+    sign_up
+
+    expect(current_path).to eq '/user/1/peeps'
+
+    num_peeps = 100
+    num_peeps.times { |i| create_peep "Test#{i} peep" }
+
+    expect(page).to have_css('td.peep',
+                             text: /^Test[0-9]+ peep/, count: num_peeps)
   end
 end
