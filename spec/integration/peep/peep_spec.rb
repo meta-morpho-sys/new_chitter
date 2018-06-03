b81b8ab0b6a148440879d5aaa5b39be814f6a404
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-04 00:56:41 +0100

    Tidying up

--- spec/integration/peep/peep_spec.rb
+++ spec/integration/peep/peep_spec.rb
@@ -20,7 +20,7 @@ describe Peep, :aggregate_failures, :db do
     end
 
     it 'returns the time of creation' do
-      expect(new_peep.timestamp).to eq frozen_time.strftime(StrMsgs::TIME)
+      expect(new_peep.timestamp).to eq frozen_time.strftime(StrMsgs::TIME_FORMAT)
     end
 
     context 'when the peep lacks an id' do
