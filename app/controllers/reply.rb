9831807bb65fb5292bbff2070cd092caafdd274b
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-08 12:43:12 +0100

    Refactoring Peep model to use Sequel::Model inheritance. Partial refactoring to Reply controller to use the new interface.

--- app/controllers/reply.rb
+++ app/controllers/reply.rb
@@ -3,11 +3,18 @@
 # Controller for path to handling replies
 class Chitter < Sinatra::Base
   get '/peeps/:id/reply' do
-    @peep = Peep.find(params[:id])
+    begin
+      @peep = Peep.with_pk!(params[:id])
+    rescue Sequel::NoMatchingRow
+      flash[:notice] = StrMsgs::NO_PEEP_FOUND
+      redirect '/'
+    end
     erb :'replies/new'
+
   end
 
   post '/replies' do
+    # TODO: Is there another way to assign these parameters? This looks ugly!
     p_id = params[:peep_id]
     u_id = current_user.id
     text = params[:text]
--- spec/features/peeps/creating_peeps_spec.rb
+++ spec/features/peeps/creating_peeps_spec.rb
@@ -1,13 +1,60 @@
 # frozen_string_literal: true
 
-feature 'New peep', :db do
-  scenario 'User can create a peep' do
-    frozen_time = Time.now
-    sign_up
-    expect(current_path).to eq '/user/1/peeps'
+# describe 'User can create a peep', type: :view do
+#   it 'finds a peep' do
+#     # Time.now.freeze
+#     #
+#     # # setup
+#     # sign_up
+#     # expect(current_path).to eq '/user/1/peeps'
+#
+#     assign :peep, Peep.new(id: 5, user_id: 3, text: "Bob's peep", created_at: Time.now)
+#
+#     render
+#     expect(rendered).to have_css('tr', text: "Bob's peep")
+#   end
+# end
 
-    create_peep "Bob's peep"
-    expect(page).to have_content frozen_time.strftime(StrMsgs::TIME)
-    expect(page).to have_content "Bob's peep"
+# feature 'New peep', :db do
+#   scenario 'User *** create a peep' do
+#     frozen_time = Time.now
+#
+#     expected_string = frozen_time.strftime(StrMsgs::TIME_FORMAT).strip
+#     sign_up
+#     expect(current_path).to eq '/user/1/peeps'
+#
+#     create_peep "Bob's peep"
+#     expect(page).to have_content expected_string
+#
+#     # frozen_time.
+#     #   strftime(StrMsgs::TIME_FORMAT).strip
+#
+#     expect(page).to have_content "Bob's peep"
+#   end
+
+
+feature 'User can create a peep', :db do
+  scenario 'User can create a peep' do
+    # setup
+    frozen_time = Timecop.freeze(Time.now)
+    user = User.create('Bob', 'bob@bob', 'pswd')
+    sign_up
+
+    peep_text = "Bob's peep"
+    create_peep peep_text
+    peep = Peep.create(user.id, peep_text)
+
+    pp peep
+
+
+    # expect(page).to have_css('tr > nth-') peep.html
   end
 end
+
+# feature 'User can create a peep', :db do
+#   scenario 'it can be created' do
+#     peep = Peep.new(id: 5, user_id: 3, text: "Bob's peep", created_at: Time.now)
+#     expect(rendered)
+#     puts peep.html
+#   end
+# end
