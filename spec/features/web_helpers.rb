1c601f7a37255f4caf84f06898a24f6eb1759b6d
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-08 14:46:22 +0100

    Some more helpers

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-def sign_up(name = 'Bob', email = 'test_1@example.com')
+def sign_up(name = 'Bob', email = 'test_1@example.com', pswd = 'password123')
   visit '/'
   click_link 'Sign up'
 
   fill_in('name', with: name)
   fill_in('email', with: email)
-  fill_in('password', with: 'password123')
+  fill_in('password', with: pswd)
   click_button 'Sign up'
 end
 
-def sign_in(email = 'test_1@example.com', password = 'password123' || user)
+def sign_in(user)
   visit '/'
   click_link 'Sign in'
 
   expect(current_path).to eq '/login/sign_in'
 
-  fill_in('email', with: email || user.email)
-  fill_in('password', with: password)
+  fill_in('email', with: user.email)
+  fill_in('password', with: 'password123')
   click_button 'Sign in'
 end
 
@@ -27,8 +27,13 @@ def create_peep(text)
   click_button 'Peep'
 end
 
+def create_with(peep)
+  click_link 'New peep'
+  fill_in('text', with: peep.text)
+  click_button 'Peep'
+end
+
 def sign_up_and_peep(name, email, text)
   sign_up name,  email
   create_peep text
 end
-
