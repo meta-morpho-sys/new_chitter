401f6ef4844c25ba1eb966219e3fadc4c8212c90
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-08 15:18:18 +0100

    Fixed a typo

--- spec/features/login/authentication_spec.rb
+++ spec/features/login/authentication_spec.rb
@@ -9,7 +9,7 @@ feature 'Signing in', :aggregated_failures, :db do
 
   context 'the user receives a warning' do
     scenario 'if tries to access without previously signing up' do
-      sign_in email: 'anna@anna'
+      sign_in email_str: 'anna@anna'
       expect(page).to have_content 'No such user.'
     end
   end
