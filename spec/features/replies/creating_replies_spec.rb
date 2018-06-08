6c31cd2d554ccad744de876e010a4d4f02929315
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-08 15:45:13 +0100

    Refactoring

--- spec/features/replies/creating_replies_spec.rb
+++ spec/features/replies/creating_replies_spec.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 feature 'Replies', :db do
-  given(:user) { User.create('Bob', 'test_1@example.com', 'password123') }
+  given(:user) { User.create('Bob', 'test_1@example.com', 'pswd123') }
   given(:other_user) { User.create('Anna', 'anna@example.com', 'password123') }
   given(:peep) { Peep.create(user_id: user.id, text: 'Destination SUMMER') }
 
   scenario 'writing a reply to a peep' do
-    sign_in(user)
+    sign_in(user_obj: user)
 
     create_with peep
 
