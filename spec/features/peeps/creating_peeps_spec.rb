246be96e6b49c56dc2cdddccf8b019ed5e43fa50
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2018-06-18 13:48:10 +0100

    Testing peep as a whole element of abstraction - table containing text and dynamically created time is dynamically.

--- spec/features/peeps/creating_peeps_spec.rb
+++ spec/features/peeps/creating_peeps_spec.rb
@@ -1,60 +1,17 @@
 # frozen_string_literal: true
 
-# describe 'User can create a peep', type: :view do
-#   it 'finds a peep' do
-#     # Time.now.freeze
-#     #
-#     # # setup
-#     # sign_up
-#     # expect(current_path).to eq '/user/1/peeps'
-#
-#     assign :peep, Peep.new(id: 5, user_id: 3, text: "Bob's peep", created_at: Time.now)
-#
-#     render
-#     expect(rendered).to have_css('tr', text: "Bob's peep")
-#   end
-# end
-
-# feature 'New peep', :db do
-#   scenario 'User *** create a peep' do
-#     frozen_time = Time.now
-#
-#     expected_string = frozen_time.strftime(StrMsgs::TIME_FORMAT).strip
-#     sign_up
-#     expect(current_path).to eq '/user/1/peeps'
-#
-#     create_peep "Bob's peep"
-#     expect(page).to have_content expected_string
-#
-#     # frozen_time.
-#     #   strftime(StrMsgs::TIME_FORMAT).strip
-#
-#     expect(page).to have_content "Bob's peep"
-#   end
-
-
+# TODO: Need to find a way to integrate the abstraction into the Capybara test.
+# TODO: The abstraction currently is expressed in erb peep.erb see line 50
 feature 'User can create a peep', :db do
-  scenario 'User can create a peep' do
+  scenario 'User c  an create a peep' do
     # setup
-    frozen_time = Timecop.freeze(Time.now)
     user = User.create('Bob', 'bob@bob', 'pswd')
     sign_up
 
     peep_text = "Bob's peep"
     create_peep peep_text
-    peep = Peep.create(user.id, peep_text)
+    peep = Peep.create(user_id: user.id, text: peep_text, created_at: Time.now)
 
-    pp peep
-
-
-    # expect(page).to have_css('tr > nth-') peep.html
+    expect(page.html).to include peep.erb
   end
 end
-
-# feature 'User can create a peep', :db do
-#   scenario 'it can be created' do
-#     peep = Peep.new(id: 5, user_id: 3, text: "Bob's peep", created_at: Time.now)
-#     expect(rendered)
-#     puts peep.html
-#   end
-# end
